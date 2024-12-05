71cd3c7b92454b7c0cddbe0ab8151eb4ca89a2cc
tehies <kulwinder.kumar@iamtechie.com>
2024-12-05 18:44:48 +0530

    third

--- src/i18n.js
+++ src/i18n.js
@@ -1,26 +1,26 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
-  .use(LanguageDetector) // Detects user language
-  .use(initReactI18next) // Passes i18n instance to react-i18next
-  .init({
-    debug: true, // Enables debugging messages in the console
-    fallbackLng: 'en', // Fallback language if detection fails
-    lng: 'en', // Default language
-    resources: {
-      en: {
-        translation: {
-          bannertext: 'Black Friday Discounts.',
-        },
-      },
-      ar: {
-        translation: {
-          bannertext: 'خصومات الجمعة السوداء.',
-        },
-      },
-    },
-  });
+// i18n
+//   .use(LanguageDetector) // Detects user language
+//   .use(initReactI18next) // Passes i18n instance to react-i18next
+//   .init({
+//     debug: true, // Enables debugging messages in the console
+//     fallbackLng: 'en', // Fallback language if detection fails
+//     lng: 'en', // Default language
+//     resources: {
+//       en: {
+//         translation: {
+//           bannertext: 'Black Friday Discounts.',
+//         },
+//       },
+//       ar: {
+//         translation: {
+//           bannertext: 'خصومات الجمعة السوداء.',
+//         },
+//       },
+//     },
+//   });
 
-export default i18n;
+// export default i18n;
